a476496a3331ffa80445fcf7d2f2797ae72bf2a1
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-02-16 16:23:20 +0200

    Implement quote repository

--- packages/quote_repository/example/quote_repository_example.dart
+++ packages/quote_repository/example/quote_repository_example.dart
@@ -1,6 +1,1 @@
-import 'package:quote_repository/quote_repository.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
--- packages/quote_repository/test/quote_repository_test.dart
+++ packages/quote_repository/test/quote_repository_test.dart
@@ -1,16 +1,13 @@
-import 'package:quote_repository/quote_repository.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(1 + 1 == 2, isTrue);
     });
   });
 }
