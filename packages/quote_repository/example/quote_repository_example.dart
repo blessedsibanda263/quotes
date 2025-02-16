55534f722197ad9dbe86c4870387e0cfad473da3
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-02-16 14:50:52 +0200

    Create quote repository package

---
+++ packages/quote_repository/example/quote_repository_example.dart
@@ -0,0 +1,6 @@
+import 'package:quote_repository/quote_repository.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/quote_repository/test/quote_repository_test.dart
@@ -0,0 +1,16 @@
+import 'package:quote_repository/quote_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
