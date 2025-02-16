e9c8c3bbfa31031cdcb95d03416e9b6cc97b4ed8
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-02-16 13:56:28 +0200

    Create dart package and install dependencies

---
+++ packages/key_value_storage/example/key_value_storage_example.dart
@@ -0,0 +1,6 @@
+import 'package:key_value_storage/key_value_storage.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/key_value_storage/test/key_value_storage_test.dart
@@ -0,0 +1,16 @@
+import 'package:key_value_storage/key_value_storage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
